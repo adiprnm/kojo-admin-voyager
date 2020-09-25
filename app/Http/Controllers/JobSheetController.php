ac8e30d666dd859dc68bcd3ac39a3c954032b0fe
Adi Purnama <adiprnm2014@gmail.com>
2020-09-25 20:43:35 +0700

    refactor JobSheetController

--- app/Http/Controllers/JobSheetController.php
+++ app/Http/Controllers/JobSheetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JobSheetHelper;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
@@ -10,10 +11,23 @@ class JobSheetController extends VoyagerBaseController
 {
     public function store(Request $request)
     {
+        Order::where('id', $request->order_id)->update([
+            'status' => $request->status
+        ]);
+        
         $request->merge([
             'code' => JobSheetHelper::generateCode($request->batch)
         ]);
 
         return parent::store($request);
+    }
+
+    public function update(Request $request, $id)
+    {
+        Order::where('id', $request->order_id)->update([
+            'status' => $request->status
+        ]);
+
+        return parent::update($request, $id);
     }
 }
