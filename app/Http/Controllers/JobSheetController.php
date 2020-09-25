2796f41bff98d84b1a968f4c5a53b565148ad73f
Adi Purnama <adiprnm2014@gmail.com>
2020-09-25 19:59:44 +0700

    add JobSheetController

---
+++ app/Http/Controllers/JobSheetController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\JobSheetHelper;
+use Illuminate\Http\Request;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+class JobSheetController extends VoyagerBaseController
+{
+    public function store(Request $request)
+    {
+        $request->merge([
+            'code' => JobSheetHelper::generateCode($request->batch)
+        ]);
+
+        return parent::store($request);
+    }
+}
